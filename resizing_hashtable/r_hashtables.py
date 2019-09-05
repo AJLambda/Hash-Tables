7e6b6feaf2969868800bfb2cc1e70ed4fab465d4
Austin James <apj2277@gmail.com>
2019-09-04 19:30:11 -0700

    tests still wont pass

--- resizing_hashtable/r_hashtables.py
+++ resizing_hashtable/r_hashtables.py
@@ -12,13 +12,11 @@ class LinkedPair:
 
 # '''
 # Fill this in
-
-# Resizing hash table
 # '''
 class HashTable:
     def __init__(self, capacity):
         self.capacity = capacity
-        self.storage = [None] * capacity
+        self.storage = [None] * capacit
 
 
 # '''
@@ -36,19 +34,42 @@ def hash(string, max):
 # '''
 # Fill this in.
 
+
 # Hint: Used the LL to handle collisions
 # '''
 def hash_table_insert(hash_table, key, value):
-    pass
-
+    # set the hash index
+    index = hash(key, hash_table.capacity)
+    # set the key value pair
+    new_pair = LinkedPair(key, value)
+    # get the current pair
+    current_pair = hash_table.storage[index]
+    # if the key/value exists and the key is different
+    if current_pair is not None:
+        if new_pair.key != current_pair.key:
+            current_pair.next = new_pair
+    else:
+        current_pair = new_pair
 
 # '''
 # Fill this in.
 
 # If you try to remove a value that isn't there, print a warning.
 # '''
+
+
 def hash_table_remove(hash_table, key):
-    pass
+     # set the hash index
+    index = hash(key, hash_table.capacity)
+    # get the current pair
+    current_pair = hash_table.storage[index]
+    # if no index exists
+    if current_pair is None:
+        # warning value doesnt exist
+        print("warning, no value for this key")
+    elif current_pair.key == key:
+        # if  index exists
+        current_pair = None
 
 
 # '''
@@ -57,33 +78,57 @@ def hash_table_remove(hash_table, key):
 # Should return None if the key is not found.
 # '''
 def hash_table_retrieve(hash_table, key):
-    pass
+     # set the hash index
+    index = hash(key, hash_table.capacity)
+    # get the current pair
+    current_pair = hash_table.storage[index]
+    # if not found
+    if current_pair is not None:
+        # retrieve the key
+        if current_pair.key == key:
+            return current_pair.value
 
 
 # '''
 # Fill this in
 # '''
 def hash_table_resize(hash_table):
-    pass
+     # doubles the size of your hash table
+    capacity = len(hash_table.storage) * 2
+    new_table = HashTable(capacity)
+    # copies all elements into the new data structure.
+    for i in range(0, len(hash_table.storage) - 1):
+        new_table.storage[i] = hash_table.storage[i]
+    return new_table
 
 
-def Testing():
-    ht = HashTable(2)
-
-    hash_table_insert(ht, "line_1", "Tiny hash table")
-    hash_table_insert(ht, "line_2", "Filled beyond capacity")
-    hash_table_insert(ht, "line_3", "Linked list saves the day!")
-
-    print(hash_table_retrieve(ht, "line_1"))
-    print(hash_table_retrieve(ht, "line_2"))
-    print(hash_table_retrieve(ht, "line_3"))
-
-    old_capacity = len(ht.storage)
-    ht = hash_table_resize(ht)
-    new_capacity = len(ht.storage)
-
-    print("Resized hash table from " + str(old_capacity)
-          + " to " + str(new_capacity) + ".")
+ht = HashTable(2)
 
 
-Testing()
+print("initial table", ht.capacity)
+
+ht2 = hash_table_resize(ht)
+
+print("resize table", ht2.capacity)
+
+
+# def Testing():
+#     ht = HashTable(2)
+
+#     hash_table_insert(ht, "line_1", "Tiny hash table")
+#     hash_table_insert(ht, "line_2", "Filled beyond capacity")
+#     hash_table_insert(ht, "line_3", "Linked list saves the day!")
+
+#     print(hash_table_retrieve(ht, "line_1"))
+#     print(hash_table_retrieve(ht, "line_2"))
+#     print(hash_table_retrieve(ht, "line_3"))
+
+#     old_capacity = len(ht.storage)
+#     ht = hash_table_resize(ht)
+#     new_capacity = len(ht.storage)
+
+#     print("Resized hash table from " + str(old_capacity)
+#           + " to " + str(new_capacity) + ".")
+
+
+# Testing()
